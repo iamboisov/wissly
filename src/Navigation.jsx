49072d86441f32273d37122660ee74cc549ddc80
Amir <amir.boisov@gmail.com>
2024-06-28 02:50:00 +0300

    added files

---
+++ src/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navigation() {
+    return <div className='Navbar'>
+    <div className='Logo'>Logo</div>
+    <div className="Links">
+        <div>Статьи</div>
+        <div>Стартапы</div>
+        <div>Бизнес-идеи</div>
+        <div>О нас</div>
+    </div>
+    <div className="Join_button">Присоединиться</div>
+    </div>
+}
