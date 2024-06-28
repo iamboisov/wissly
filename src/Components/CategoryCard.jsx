cda6d6a307ed12162485933269addeaedea4bda8
Amir <amir.boisov@gmail.com>
2024-06-28 13:56:01 +0300

    added category cards

---
+++ src/Components/CategoryCard.jsx
@@ -0,0 +1,71 @@
+import z_white from '../assets/z_white.png'
+import link_img from '../assets/link.png'
+
+export default function CategoryCard(props) {
+    switch (props.variant) {
+        case 1:
+            return (
+                <>
+                    <div className="categoryCard1">
+                        <div className="categoryCardTop">
+                            <p>17 статей</p>
+                            <a href="#"><img src={link_img} alt="" /></a>
+                        </div>
+        
+                        <div className="categoryCardBottom">
+                            <h5>{props.title}</h5>
+                            <p>Статьи, которые помогут выстроить маркетинг</p>
+                        </div>
+                    </div>
+                </>
+            )
+        case 2:
+            return (
+                <>
+                    <div className="categoryCard2">
+                        <div className="categoryCardTop">
+                            <p>12 статей</p>
+                            <a href="#"><img src={link_img} alt="" /></a>
+                        </div>
+        
+                        <div className="categoryCardBottom">
+                            <h5>Финансы</h5>
+                            <p>Статьи, которые помогут выстроить маркетинг</p>
+                        </div>
+                    </div>
+                </>
+            )
+        case 3:
+            return (
+                <>
+                    <div className="categoryCard3">
+                        <div className="categoryCardTop">
+                            <p>9 статей</p>
+                            <a href="#"><img src={link_img} alt="" /></a>
+                        </div>
+        
+                        <div className="categoryCardBottom">
+                            <h5>Управление</h5>
+                            <p>Статьи, которые помогут выстроить маркетинг</p>
+                        </div>
+                    </div>
+                </>
+            )
+        case 4:
+            return (
+                <>
+                    <div className="categoryCard4">
+                        <div className="categoryCardTop">
+                            <p>10 статей</p>
+                            <a href="#"><img src={link_img} alt="" /></a>
+                        </div>
+        
+                        <div className="categoryCardBottom">
+                            <h5>Стратегия</h5>
+                            <p>Статьи, которые помогут выстроить маркетинг</p>
+                        </div>
+                    </div>
+                </>
+            )
+    }
+}
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,15 @@
 import Navigation from './Components/Navigation'
+import CategoryCard from './Components/CategoryCard'
 
 export default function Home() {
     return (
         <>
         <Navigation />
+        <div className='divider'></div>
+        <CategoryCard variant={1} amount={17} title="Маркетинг" text="Статьи, которые помогут выстроить маркетинг"/>
+        <CategoryCard variant={2} amount={12} title="Финансы" text="Статьи, которые помогут выстроить маркетинг"/>
+        <CategoryCard variant={3} amount={9} title="Управление" text="Статьи, которые помогут выстроить маркетинг"/>
+        <CategoryCard variant={4} amount={5} title="Стратегия" text="Статьи, которые помогут выстроить маркетинг"/>
         </>
     )
 }
